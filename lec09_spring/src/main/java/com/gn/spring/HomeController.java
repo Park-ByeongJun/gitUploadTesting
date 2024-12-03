b5e3bf761de55e0a233a088a30a68700cc09179f
pbkan <pbkan4@gmail.com>
2024-12-03 18:50:35 +0900

    [박병준]Git Upload Testing

---
+++ lec09_spring/src/main/java/com/gn/spring/HomeController.java
@@ -0,0 +1,24 @@
+package com.gn.spring;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	// 클라이언트가 요청한 서비스 주소와 메소드를 연결
+	// http://localhost:8090
+	// http://localhost:8090/ 자바 인식 다름
+	
+	private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
+	
+	@RequestMapping(value= {"","/"},method=RequestMethod.GET)
+	public String home() {
+		// /WEB-INF/views/home.jsp 와 같음
+		LOGGER.info("게시판 프로그램 시작");
+		return "home";
+	}
+}
